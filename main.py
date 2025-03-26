2eddf179f00f7dad8995a36af4700d47d467f59e
Parshan <parshan.mm@gmail.com>
2025-03-26 09:45:02 -0700

    create profile is done.

--- main.py
+++ main.py
@@ -27,12 +27,43 @@ def join_channel_markup():
     markup.add(button, check_btn)
     return markup
 
+def profile_create_markup():
+    markup = InlineKeyboardMarkup(row_width=1)
+    button_change = InlineKeyboardButton(text='create profile📰', callback_data='create prof')
+    markup.add(button_change)
+    return markup
+
 def main_menu():
     markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
     chat_btn = KeyboardButton('chat with someone unknow')
     profile_btn = KeyboardButton('Your Profile')
     markup.add(chat_btn, profile_btn)
     return markup
+
+def create_profile(message):
+    bot.send_message(message.chat.id, 'please enter your name.')
+    bot.register_next_step_handler(message, name_handel)
+
+def name_handel(message):
+    name = message.text
+    bot.send_message(message.chat.id, 'Please enter your gender (you can choose between male, female or don\'t say)')
+    bot.register_next_step_handler(message, gender_handel, name=name)
+
+def gender_handel(message, name):
+    gender = message.text
+    bot.send_message(message.chat.id, 'Please enter your age')
+    bot.register_next_step_handler(message, age_handel, name=name, gender=gender)
+
+def age_handel(message, name, gender):
+    try:
+        age = int(message.text)
+        bot.send_message(message.chat.id, 'Please enter your age')
+        db.update_user(user_id, {'first_name':name, 'gender':gender, 'age':age})
+    except ValueError:
+        bot.send_message(message.chat.id, 'please enter a number')
+        bot.send_message(message.chat.id, 'Please enter your age')
+        bot.register_next_step_handler(message, age_handel, name=name, gender=gender)
+
 
 @bot.message_handler(commands=['start'])
 def Welcome(message):
@@ -55,7 +86,16 @@ def Welcome(message):
 
 @bot.message_handler(func=lambda message:message.text == 'Your Profile')
 def profile(message):
-    pass
+    try:
+        global prof_message
+        prof_message = message
+        bot.reply_to(message, 'Your profile👇')
+        dat = db.get_info(user_id)
+        prof = f'name: {dat[2]}\n gender:{dat[3]}\n age:{str(dat[4])}'
+        bot.send_message(message.chat.id, prof)
+    except TypeError:
+        prof = f'name: unknow\n gender: unknow\n age: unknow'
+        bot.send_message(message.chat.id, prof, reply_markup=profile_create_markup())
 
 @bot.callback_query_handler(lambda call:True)
 def callback_query(call):
@@ -69,5 +109,7 @@ def callback_query(call):
         else:
             bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
                                    text='Please join the channel', reply_markup=join_channel_markup())
+    if call.data == 'create prof':
+        create_profile(prof_message)
 
 bot.polling()
