211613eb20250d0999e92ddfb83b1695a0550dd9
Parshan <parshan.mm@gmail.com>
2025-04-07 12:55:55 -0700

    chat with boy is done.

--- main.py
+++ main.py
@@ -268,6 +268,7 @@ def find_some(message):
                     bot.send_message(match[1], f'You are connected to {ud[2]}\ngender: {ud[3]}\nage: {ud[4]}', reply_markup=disconnet_markup())
                     bot.send_message(message.chat.id, f'You are connected to {match[2]}\ngender: {match[3]}\nage: {match[4]}', reply_markup=disconnet_markup())
                     conecteds.append((ud[1], match[1]))
+                    
                 else:
                     bot.send_message(message.chat.id, 'You are in the pending list')
                     pending_rand_list.append(match)
